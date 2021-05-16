49e5caa567a4636f86d76601dc6c9cf16715429a
Ashley Noel Hinton <ashley@canadia.co.nz>
2021-05-17 11:40:28 +1200

    Initial commit

---
+++ desktop/src/nz/co/canadia/watchedclock/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package nz.co.canadia.watchedclock.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import nz.co.canadia.watchedclock.WatchedClock;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new WatchedClock(), config);
+	}
+}
---
+++ ios/src/nz/co/canadia/watchedclock/IOSLauncher.java
@@ -0,0 +1,22 @@
+package nz.co.canadia.watchedclock;
+
+import org.robovm.apple.foundation.NSAutoreleasePool;
+import org.robovm.apple.uikit.UIApplication;
+
+import com.badlogic.gdx.backends.iosrobovm.IOSApplication;
+import com.badlogic.gdx.backends.iosrobovm.IOSApplicationConfiguration;
+import nz.co.canadia.watchedclock.WatchedClock;
+
+public class IOSLauncher extends IOSApplication.Delegate {
+    @Override
+    protected IOSApplication createApplication() {
+        IOSApplicationConfiguration config = new IOSApplicationConfiguration();
+        return new IOSApplication(new WatchedClock(), config);
+    }
+
+    public static void main(String[] argv) {
+        NSAutoreleasePool pool = new NSAutoreleasePool();
+        UIApplication.main(argv, null, IOSLauncher.class);
+        pool.close();
+    }
+}
