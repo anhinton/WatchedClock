5641cb02d385f12974010a0ede59a164d1786771
Ashley Noel Hinton <ashley@canadia.co.nz>
2021-05-31 14:48:35 +1200

    Store currentScreen in preferences when switching; load last currentScreen on app start

--- core/src/nz/co/canadia/watchedclock/WatchedClock.java
+++ core/src/nz/co/canadia/watchedclock/WatchedClock.java
@@ -3,13 +3,10 @@ package nz.co.canadia.watchedclock;
 import com.badlogic.gdx.Game;
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.Preferences;
+import com.badlogic.gdx.Screen;
 import com.badlogic.gdx.assets.AssetManager;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
-import com.badlogic.gdx.scenes.scene2d.Actor;
 import com.badlogic.gdx.scenes.scene2d.ui.Skin;
-import com.badlogic.gdx.scenes.scene2d.ui.Table;
-import com.badlogic.gdx.scenes.scene2d.ui.TextButton;
-import com.badlogic.gdx.scenes.scene2d.utils.ChangeListener;
 import com.badlogic.gdx.utils.I18NBundle;
 
 import java.util.Date;
@@ -42,12 +39,31 @@ public class WatchedClock extends Game {
 		bundle = manager.get("i18n/Bundle", I18NBundle.class);
 		skin = manager.get("skin/uiskin.json", Skin.class);
 
-		this.setScreen(new StopwatchScreen(this));
+		Screen screen;
+		switch (preferences.getString("currentScreen", "ClockScreen")) {
+			case "AlarmScreen":
+				screen = new AlarmScreen(this);
+				break;
+			case "StopwatchScreen":
+				screen = new StopwatchScreen(this);
+				break;
+			default:
+				screen = new ClockScreen(this);
+				break;
+		}
+		this.setScreen(screen);
 	}
 
 	@Override
 	public void render () {
 		super.render(); // important!
+	}
+
+	@Override
+	public void setScreen(Screen screen) {
+		super.setScreen(screen);
+		preferences.putString("currentScreen", screen.getClass().getSimpleName());
+		preferences.flush();
 	}
 
 	@Override
